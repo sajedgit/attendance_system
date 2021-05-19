565055f97ead94657fb48252ee9eae6a31db41da
sajed <sajedaiub@gmail.com>
2021-05-19 14:35:43 +0600

    add password to add emp from admin

--- admin/employee_add.php
+++ admin/employee_add.php
@@ -5,6 +5,8 @@
 		$firstname = $_POST['firstname'];
 		$lastname = $_POST['lastname'];
 		$email = $_POST['email'];
+        $password="123456";
+        $password = password_hash($password, PASSWORD_DEFAULT);
 		$address = $_POST['address'];
 		$birthdate = $_POST['birthdate'];
 		$contact = $_POST['contact'];
@@ -27,11 +29,18 @@
 		}
 		$employee_id = substr(str_shuffle($letters), 0, 3).substr(str_shuffle($numbers), 0, 9);
 		//
-		$sql = "INSERT INTO employees (employee_id, email, firstname, lastname, address, birthdate, contact_info, gender, department_id, position_id, schedule_id, photo, created_on) 
-				VALUES ('$employee_id','$email', '$firstname', '$lastname', '$address', '$birthdate', '$contact', '$gender','$department', '$position', '$schedule', '$filename', NOW())";
+		$sql = "INSERT INTO employees (employee_id, email,password, firstname, lastname, address, birthdate, contact_info, gender, department_id, position_id, schedule_id, photo, created_on) 
+				VALUES ('$employee_id','$email','$password', '$firstname', '$lastname', '$address', '$birthdate', '$contact', '$gender','$department', '$position', '$schedule', '$filename', NOW())";
 		if($conn->query($sql)){
 			$_SESSION['success'] = 'Employee added successfully';
-		}
+            $email=$email;
+            $subject="Your account has been created to hr.synesisit.info";
+            $header="From: hr.synesisit.info";
+            $content="Your new account has been created to hr.synesisit.info and Your password is <b>$password</b>. <br/> ";
+            $content.="<br/> PLEASE CHANGE THE PASSWORD after login to this system ( http://hr.synesisit.info/attendance ) ";
+            mail($email, $subject, $content, $header);
+
+        }
 		else{
 			$_SESSION['error'] = $conn->error;
 		}

2abbdb12f0e764bc3472278a867cafef1ab8589e
sajed <sajedaiub@gmail.com>
2021-05-19 11:10:03 +0600

    update login page

--- emp/index.php
+++ emp/index.php
@@ -40,13 +40,24 @@
 
 			<form action="login.php" method="POST">
 			<div class="form-group has-feedback">
-				<input type="text" class="form-control" name="email" placeholder="input Email" required autofocus>
+				<input type="text" class="form-control" name="email" placeholder="Email" required autofocus> 
+				<div class="input-group-append">
+				  <span class="input-group-text" id="basic-addon2" style="color:darkslategrey;">Hint: @synesisit.info</span>
+			    </div>
+   
 				<span class="glyphicon glyphicon-user form-control-feedback"></span>
+				
 			</div>
+
+			
 			<div class="form-group has-feedback">
-			<input type="password" class="form-control" name="password" placeholder="input Password" required>
+			<input type="password" class="form-control" name="password" placeholder="Password" required>
 			<span class="glyphicon glyphicon-lock form-control-feedback"></span>
 			</div>
+			
+		
+			
+			
 			<div class="row">
 				<div class="col-xs-4">
 					<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
